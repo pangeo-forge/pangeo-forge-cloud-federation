24a09c72b93324becde31722c16bed9d0ffa4588
ranchodeluxe <greg.corradini@gmail.com>
2023-12-06 07:53:53 -0800

    remove default hardcoded namespace and other things

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     # FIXME: Investigate if we need the dynamodb locking here?
-    bucket = "pangeo-forge-federation-gcorradini-tfstate-v3"
+    bucket = "pangeo-forge-federation-tfstate"
     key    = "terraform"
     region = "us-west-2"
   }
