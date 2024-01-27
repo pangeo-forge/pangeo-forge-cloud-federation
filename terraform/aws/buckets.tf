5fd8da106dc6faeb19e92bdd7d17766eee6a1e65
Timothy Hodson <34148978+thodson-usgs@users.noreply.github.com>
2024-01-27 13:34:55 -0600

    Remove flink-cache bucket

--- terraform/aws/buckets.tf
+++ terraform/aws/buckets.tf
@@ -1,8 +1,3 @@
-resource "aws_s3_bucket" "flink_store" {
-  bucket = "${var.cluster_name}-flink-store"
-  force_destroy = true
-}
-
 resource "aws_s3_bucket" "buckets" {
   for_each = toset(var.buckets)
   bucket   = each.key
