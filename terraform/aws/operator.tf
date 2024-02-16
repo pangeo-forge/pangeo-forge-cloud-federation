bf18fd520228484df5bfe9b31dfc566020d403b0
thodson <thodson@usgs.gov>
2024-02-16 13:58:25 -0600

    Fix typo in flink config

--- terraform/aws/operator.tf
+++ terraform/aws/operator.tf
@@ -50,8 +50,8 @@ resource "helm_release" "flink_operator" {
         "prometheus.io/port" : "9999"
       }
       # Configure node groups
-      "kubernetes.jobmanager.node-selector" : {"eks.amazonaws.com/nodegroup" : "$(var.node_groups.jobmanager.node_group_name)"}
-      "kubernetes.taskmanager.node-selector" : {"eks.amazonaws.com/nodegroup" : "${var.node_groups.taskmanager.node_group_name}"}
+      "kubernetes.jobmanager.node-selector" : {"eks.amazonaws.com/nodegroup" : var.node_groups.jobmanager.node_group_name}
+      "kubernetes.taskmanager.node-selector" : {"eks.amazonaws.com/nodegroup" : var.node_groups.taskmanager.node_group_name}
     })
   }
 
